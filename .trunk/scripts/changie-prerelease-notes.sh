afc2aad9f795ce565885442646f7d2d8a2148460
Sheldon Hull <sheldonhull@users.noreply.github.com>
2023-06-06 14:30:40 -0500

    chore(lint): add fmt step after changie prerelease generation

--- .trunk/scripts/changie-prerelease-notes.sh
+++ .trunk/scripts/changie-prerelease-notes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH"
-changie batch auto --keep --prerelease 'PRERELEASE' --force && changie merge
+changie batch auto --keep --prerelease 'PRERELEASE' --force && changie merge && trunk fmt --filter=prettier
